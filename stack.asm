6ff3bb6c668d73a1c5959ef09d42ff8c75e5f7d5
Yih-Chun Hu <git@matrii.com>
2023-01-26 15:21:29 -0600

    l4

--- stack.asm
+++ stack.asm
@@ -17,6 +17,10 @@
 ; stack pointer init
 LEA R6, BOT_OF_STACK_PLUS1 ; initialize the stack pointer
 
+; Register initialization
+; R4 address of the character currently being analyzed
+; R2 -'('
+; R3 -'['
 LEA R4, TEST_STRING
 LD R2, OPENPAREN
 NOT R2, R2
@@ -25,60 +29,72 @@ LD R3, OPENBRACKET
 NOT R3, R3
 ADD R3, R3, #1
 
-; R2 == -'('
-; R3 == -'['
-; '(' + 1 == ')'
-; '[' + 1 == ']'
+; R1 current character
+; R0 temporary register
 LOOPTOP
-	LDR R1, R4, #0
-	BRz END_OF_STRING
-	ADD R0, R1, R2
-	BRz HANDLE_OPEN ; if zero, then open paren
-	ADD R0, R0, #-1
+	LDR R1, R4, #0    ; load the current character
+	BRz END_OF_STRING ; if NUL, then at end of string
+	ADD R0, R1, R2    ; computes char-'('
+	BRz HANDLE_OPEN   ; if zero, then open paren
+	ADD R0, R0, #-1   ; compute char-')'
 	BRz HANDLE_CLOSED_PAREN ; if zero, then closed paren
-	ADD R0, R1, R3
-	BRz HANDLE_OPEN ; if zero, then open bracket
-	ADD R0, R0, #-1 ; needs to be 2
+	ADD R0, R1, R3    ; compute char-'['
+	BRz HANDLE_OPEN  ; if zero, then open bracket
+	ADD R0, R0, #-2  ; compute char-']' (']' - '[' == 2)
 	BRz HANDLE_CLOSED_BRACKET ; if zero, then closed bracket
+	ADD R4, R4, #1   ; ignore the non-parenthetical character
+	BRnzp LOOPTOP
 	
 HANDLE_OPEN
-	ADD R0, R1, #0 ; R0 <- R1+0 == R1
-	JSR PUSH
-	ADD R4, R4, #1
+	ADD R0, R1, #0   ; push the current char (now in R1, needs to be in R0)
+	                 ; R0 <- R1+0 == R1
+	JSR PUSH	 ; push R0 onto the stack
+	ADD R5, R5, #0   ; check R5 to make sure we didn't overflow
+	BRp STRING_TOO_LONG
+	ADD R4, R4, #1   ; move to next character
 	BRnzp LOOPTOP
 
 HANDLE_CLOSED_PAREN
-	JSR POP
-	ADD R0, R0, R2 ; 0 if popped a '('
-	BRnp OOPS_FAILED
-	ADD R4, R4, #1
+	JSR POP		 ; pop and check that we match '('
+	ADD R5, R5, #0	 ; set condition code based on R5 (success/failure)
+	BRp OOPS_FAILED ; POP on empty stack
+	ADD R0, R0, R2   ; compute popped value - '('
+	BRnp OOPS_FAILED ; non-zero if '(' is not matched
+	ADD R4, R4, #1   ; move to next character
 	BRnzp LOOPTOP
 
 HANDLE_CLOSED_BRACKET
-	LDR R0, R4, #0
-	JSR POP
-	ADD R0, R0, R3 ; 0 if popped a '['
-	BRnp OOPS_FAILED
-	ADD R4, R4, #1
+	JSR POP		 ; pop and check that we match '['
+	ADD R5, R5, #0	 ; set condition code based on R5 (success/failure)
+	BRp OOPS_FAILED  ; POP on empty stack
+	ADD R0, R0, R3   ; compute popped value - '[' (0 if popped a '[')
+	BRnp OOPS_FAILED ; non-zero if '[' is not matched
+	ADD R4, R4, #1   ; move to next character
 	BRnzp LOOPTOP
 
 END_OF_STRING
-	LEA R0, SUCCESS_STRING
-	PUTS
-	HALT
+	JSR POP		 ; check if anything is still on the stack
+	ADD R5, R5, #0   ; if pop was successful, something is on the stack
+	BRz OOPS_FAILED  ; ...so fail
+	LEA R0, SUCCESS_STRING ; indicate that parens match
+	BRnzp PUTS_HALT
 
 OOPS_FAILED
-	LEA R0, FAILURE_STRING
+	LEA R0, FAILURE_STRING ; complain
+	BRnzp PUTS_HALT
+
+STRING_TOO_LONG
+	LEA R0, OVERFULL_STACK
+PUTS_HALT
 	PUTS
 	HALT
 
 OPENPAREN .STRINGZ "("
 OPENBRACKET .STRINGZ "["
-TEST_STRING .STRINGZ "([])"
-.STRINGZ "([])[()][]()"
+TEST_STRING .STRINGZ "[([])([()][])]()"
 SUCCESS_STRING .STRINGZ "Your parentheses match"
 FAILURE_STRING .STRINGZ "Your parentheses don't match"
-
+OVERFULL_STACK .STRINGZ "Our stack is too small to check your string"
 
 ; PUSH - push R0 to the top of the stack
 ; R5 will be 0 if successful, and 1 otherwise
